17fe708fccfedf9bddcd8aae01cdcfb7a60e4cd0
Connor Adams <connorvadams@gmail.com>
2023-09-18 07:38:56 -0600

    First draft of simple liker, initializes DOM variables and outlines functionality needed.

--- main.js
+++ main.js
@@ -4,8 +4,29 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+//Initialize variables for DOM elements that will need to be manipulated.
+const likeGlyph = document.querySelectorAll('.like-glyph')
+const errorBanner = document.querySelector('#modal')
 
+//Added to heart buttons w/ an event listener. Pings the server and changes the content of the span w/ class 'like-glyph'
+const addLike = () => {
+  //Invoke mimicServerCall to attempt to fetch a response from server.
 
+  //If successful, change 'like-glyph' text content.
+
+    //Then add .activated-heart CSS class to make it red.
+
+  //If unsuccessful, display error modal.
+
+    //Make modal hidden again after 3 seconds.
+}
+
+//Defines behavior for when user clicks on heart (using .toggle()), dependent on whether or not it's already been liked.
+const heartClick = () => {
+  //If the heart class is activated, remove that class and change the glpyh.
+
+  //Else add that class and change the glyph.
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
