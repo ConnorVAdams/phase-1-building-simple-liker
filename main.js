fa323df7b747a5f006036bdf5d28b1e46366ea4c
Connor Adams <connorvadams@gmail.com>
2023-09-18 08:22:04 -0600

    Toggles style behavior of heart on click event.

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const FULL_HEART = '♥'
 // Your JavaScript code goes here!
 
 //Initialize variables for DOM elements that will need to be manipulated.
-const likeGlyphs = [...document.querySelectorAll('.like-glyph')]
-const errorBanner = document.querySelector('#modal')
+const likeGlyphs = [...document.querySelectorAll('.like-glyph')];
+const errorBanner = document.querySelector('#modal');
 
 //Adds an event listener that sends an HTTP request when the like button is clicked.
 // likeGlyphs.forEach((element) => {
@@ -15,32 +15,21 @@ const errorBanner = document.querySelector('#modal')
 //   })
 // })
 
-//Adds an event listener that changes the text and class of the heart.
+// TODO Can the first line of this callback function be refactored to use .toggle()?
+//Adds an event listener to like-glyph that changes the text and class of the heart.
 likeGlyphs.forEach((element) => {
   element.addEventListener('click', (e) => {
-    e.target.textContent === FULL_HEART ? e.target.textContent = EMPTY_HEART : e.target.textContent = FULL_HEART;
-  })
-})
+    e.target.textContent === FULL_HEART ? e.target.textContent = EMPTY_HEART : e.target.textContent = FULL_HEART; //Toggles heart between empty and full.
+    e.target.classList.toggle('activated-heart') //Toggles heart between red and not red.
+  });
+});
 
-//Defines behavior for when user clicks on heart (using .toggle()), dependent on whether or not it's already been liked.
-// const changeGlyph = ('click', (e) => {
-//   //If the heart class is activated, remove that class and change the glpyh.
-//   console.log(e.target)
-//   //Else add that class and change the glyph.
-// })
-
-//Added to heart buttons w/ an event listener. Pings the server and changes the content of the span w/ class 'like-glyph'
-const addLike = () => {
-  //Invoke mimicServerCall to attempt to fetch a response from server.
-
-  //If successful, change 'like-glyph' text content.
-
-    //Then add .activated-heart CSS class to make it red.
-
-  //If unsuccessful, display error modal.
-
-    //Make modal hidden again after 3 seconds.
+//Adds an event listener to the error modal which toggles show/hide and removes it after 3 seconds if shown.
+const toggleErrorModal = () => {
+  errorBanner.toggle('hidden')
 }
+
+errorBanner.setTimeOut
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
